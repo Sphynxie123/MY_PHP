1f91dbce738ab75ad382b259bec35d1a2e16f4ef
Mantas <mantass.lukasevicius@gmail.com>
2022-05-26 19:01:26 +0300

    js2

---
+++ js2/operators/app.js
@@ -0,0 +1,5 @@
+let str = 'Hello world';
+
+let atherStr = str;
+
+console.log(str,atherStr);
---
+++ js2/string/app.js
@@ -0,0 +1,31 @@
+// let strOne = 'Hello';
+// let strTwo = 'World';
+// let strThree = '!';
+
+// console.log(strOne, strTwo, strThree);
+// console.log(strOne + strTwo + strThree);
+// console.log(strOne + ' ' + strTwo + strThree);
+// console.log(`${strOne} ${strTwo} !`);
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+// Naudodami dvigubas (‘’ arba “”) kabutes, console.log metodo viduje parašykite tokius sakinius:
+// I am a "double quoted" string inside "double quotes„; \n - NAUJA EILUTE
+// FirstLine;	\ - nauja eilute
+// \SecondLine;	\\ - uzsimeta  vienas \
+// ThirdLine.
+// \t - tabas console.log('\tI am a "double quoted" string inside "double quotes„;\nFirstLine;\n\\SecondLine;\nThirdLine.');
+
+// console.log('I am a "double quoted" string inside "double quotes„;\nFirstLine;\n\\SecondLine;\nThirdLine.');
+// // arba
+// let list = "\tI am a \"double quoted\" string inside \"double quotes\"\n" +
+//             "FirstLine\n" +
+//             "\\SecondLine\n" +
+//             "ThirdLine";
+// console.log(list);
+// // arba
+// console.log(
+//     'I am a "double quoted" string inside "double quotes „\n' +
+//     "FirstLine\n" +
+//     'ThirdLine."'
+//   );
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
