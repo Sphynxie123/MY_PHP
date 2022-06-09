f0bc4dfc83902614dddd80d38798273fa73af49e
Mantas <mantass.lukasevicius@gmail.com>
2022-06-09 18:27:19 +0300

    DOM

--- js/DOM/app.js
+++ js/DOM/app.js
@@ -3,4 +3,4 @@ console.log(document.getElementById('age-table'));
 console.log(document.getElementById('age'));
 console.log(document.getElementsByTagName('label'));
 console.log(document.getElementById('form').children[1]);
-console.log(document.getElementById('form').children[9]);
+console.log(document.getElementById('form').children[11]);
