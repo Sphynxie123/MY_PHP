70778ba56d0533f846d70f7ef7f91f8ffb8ee912
Mantas <mantass.lukasevicius@gmail.com>
2022-06-13 20:07:40 +0300

    window

--- js/DOM/2022-06-09/3rd/app.js
+++ js/DOM/2022-06-09/3rd/app.js
@@ -3,8 +3,10 @@ const heightOutput = document.querySelector('#height');
 const widthOutput = document.querySelector('#width');
 
 function reportWindowSize() {
-  heightOutput.textContent = window.innerHeight;
-  widthOutput.textContent = window.innerWidth;
+    if (heightOutput && widthOutput){
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth;
+    }
 }
 
 window.onresize = reportWindowSize;
