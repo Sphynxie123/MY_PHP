52a6c286ae6d43336906d9afe4463d0b1a9942fd
Mantas <mantass.lukasevicius@gmail.com>
2022-06-13 19:58:29 +0300

    window

--- js/DOM/2022-06-09/2nd/app.js
+++ js/DOM/2022-06-09/2nd/app.js
@@ -1,32 +1,33 @@
 // ---------------------------------------------------ID-------------------------------------------------------------------------------
 
-// const btn = document.querySelector('button');
-// // const firstInput = document.querySelector('first');
-// // const secondInput = document.queryCommandValue('second');
+const btn = document.querySelector('button');
+const first = document.querySelector('#first');
+const second = document.querySelector('#second');
 
-// if (btn) {
-//     // sukuriam Eventa, kuris veikia paspaudus 'click' iškviečia funkcija button
-//     btn.addEventListener('click', button)
-// }
-// function button(){
-//     // pasirenkam pagal id inputa second reikšme ir prilyginam iš kurio id perkelti su reikšme, šiuo metu #first
-//     // #second prilygynam pirmai reikšmei #first
-//     document.querySelector('#second').value = document.querySelector('#first').value;    
-// }
-// ---------------------------------------------------Name-------------------------------------------------------------------------------
-const copyInput =  document.getElementsByName('copy');
-
-const pasteInput = document.getElementsByName('paste');
-
-const buttonCopy = document.querySelector('button')
-
-function copyPaste (){
-    pasteInput[0].value = copyInput[0].value;
-    pasteInput?.pasteInput;
-    copyInput?.copyInput;
+if (btn) {
+    // sukuriam Eventa, kuris veikia paspaudus 'click' iškviečia funkcija button
+    btn.addEventListener('click', button)
 }
+function button(){
+    if (second && first) {
+    // pasirenkam pagal id inputa second reikšme ir prilyginam iš kurio id perkelti su reikšme, šiuo metu #first
+    // #second prilygynam pirmai reikšmei #first
+        second.value = first.value; 
+    }   
+}
+// ---------------------------------------------------Name-------------------------------------------------------------------------------
+// const copyInput =  document.getElementsByName('copy');
 
+// const pasteInput = document.getElementsByName('paste');
 
-buttonCopy?.addEventListener('click', copyPaste)
+// const buttonCopy = document.querySelector('button')
+
+// function copyPaste (){
+//     // patikrinimas pirmas masyvo elementas [0] in mygtukas kuriame atsiranda testas ir [0], i6 kurio paduodama
+//     if (0 in pasteInput && 0 in copyInput) {
+//         pasteInput[0].value = copyInput[0].value;
+//     }
+// }
+// buttonCopy?.addEventListener('click', copyPaste);
 
 // ?. if
---
+++ js/DOM/3rd/app.js
@@ -0,0 +1,10 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
