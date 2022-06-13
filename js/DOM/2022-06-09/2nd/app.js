698a6405ab29acbf8123e626fe271c85d5e3c3a5
Mantas <mantass.lukasevicius@gmail.com>
2022-06-13 18:00:18 +0300

    dom

---
+++ js/DOM/2022-06-09/2nd/app.js
@@ -0,0 +1,30 @@
+// ---------------------------------------------------ID-------------------------------------------------------------------------------
+
+// const btn = document.querySelector('button');
+// // const firstInput = document.querySelector('first');
+// // const secondInput = document.queryCommandValue('second');
+
+// if (btn) {
+//     // sukuriam Eventa, kuris veikia paspaudus 'click' iškviečia funkcija button
+//     btn.addEventListener('click', button)
+// }
+// function button(){
+//     // pasirenkam pagal id inputa second reikšme ir prilyginam iš kurio id perkelti su reikšme, šiuo metu #first
+//     // #second prilygynam pirmai reikšmei #first
+//     document.querySelector('#second').value = document.querySelector('#first').value;    
+// }
+// ---------------------------------------------------Name-------------------------------------------------------------------------------
+const copyInput =  document.getElementsByName('copy');
+
+const pasteInput = document.getElementsByName('paste');
+
+const buttonCopy = document.querySelector('button')
+
+function copyPaste (){
+    pasteInput[0].value = copyInput[0].value;
+}
+
+
+buttonCopy?.addEventListener('click', copyPaste)
+
+// ?. if
